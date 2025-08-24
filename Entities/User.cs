36652745b79fc8c6f569a8f8ffcc624417455c89
Desimo <szyfra@gmail.com>
2025-08-24 20:15:26 +0200

    custom migration script
    custom migration script

--- Entities/User.cs
+++ Entities/User.cs
@@ -5,8 +5,7 @@ namespace MyBoards.Entities
     {
         // User properties
         public Guid Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string FullName { get; set; }
         public string Email { get; set; }
 
         // Reference do Address entity
