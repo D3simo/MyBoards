5a7a6d556cb832bd0059fbc6596750b73a4a1158
Desimo <szyfra@gmail.com>
2025-08-25 23:41:06 +0200

    seeding
    seeding

--- Program.cs
+++ Program.cs
@@ -30,9 +30,42 @@ void ApplyPendingMigrations(IServiceProvider services)
     {
         dbContext.Database.Migrate();
     }
+
+    //List of Users
+    var users = dbContext.Users.ToList();
+    if (!users.Any())
+    {
+        var user1 = new User()
+        {
+            Id = Guid.NewGuid(),
+            FullName = "John Doe",
+            Email = "johndoe@gmail.com",
+            Address = new Address()
+            {
+                Id = Guid.NewGuid(),
+                Street = "123 Main St",
+                City = "Anytown",
+            }
+        };
+
+        var user2 = new User()
+        {
+            Id = Guid.NewGuid(),
+            FullName = "John Doe 2",
+            Email = "johndoe2@gmail.com",
+            Address = new Address()
+            {
+                Id = Guid.NewGuid(),
+                Street = "456 Main St",
+                City = "Anytown",
+            }
+        };
+
+        dbContext.Users.AddRange(user1, user2);
+        dbContext.SaveChanges();
+    }
 }
 
 // Call the function before app.Run()
 ApplyPendingMigrations(app.Services);
-
 app.Run();
